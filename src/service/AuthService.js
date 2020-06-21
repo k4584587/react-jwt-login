1d10f5df88e03f0132ccf42bf8d074823fa79451
k4584587 <k4584587@naver.com>
2020-06-21 14:58:21 +0900

    XSRF-TOKEN 토큰 로그인 추가

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -2,16 +2,26 @@ import axios from "axios";
 
 import { API_BASE_URL } from '../config/APIConfig';
 
+//import  Cookie  from  'js-cookie';
+
 class AuthService {
 
     async login(username, password) { //로그인 함수
+
         return axios
             .post(API_BASE_URL + "auth/signin", {
                 username,
                 password
+            }, {
+                withCredentials: true,
+                xsrfCookieName: "XSRF-TOKEN",
+                xsrfHeaderName: "X-XSRF-TOKEN"
+                //
             }).then(res => {
                 localStorage.setItem("user", JSON.stringify(res.data)); //로그인 정보를 세션에 저장
                 return res.data;
+            }).catch(error => {
+                console.log(error.toString())
             })
     }
 
